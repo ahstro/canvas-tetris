3f10c7b5f8d3ea6c26c4dc1bac5d1915564f5c02
LumenTeun <teun@lutic.org>
2014-06-21 03:46:29 +0100

    Made variables for stroke/fill values.

--- render.js
+++ render.js
@@ -1,7 +1,13 @@
+// Variables
 var canvas = document.getElementsByTagName('canvas')[0];
 var context = canvas.getContext('2d');
 var width = 300, height = 600;
 var block_width = width / cols, block_height = height / rows;
+
+var minoStroke = 'black';
+var gridStroke = '#e4e4e4';
+var gridFill = '#eeeeee';
+
 
 // Draw a single square at (x, y)
 function drawBlock(x,y){
@@ -19,14 +25,14 @@ function render(){
         for(var y = 0; y < rows; ++y){
             // If there's a mino there, render it...
             if(board[y][x]){
-                context.strokeStyle = 'black';
+                context.strokeStyle = minoStroke;
                 context.fillStyle = colors[board[y][x]-1];
                 drawBlock(x,y);
             }
             // ...otherwise, render the background grid.
             else{
-                context.fillStyle = '#eeeeee';
-                context.strokeStyle = '#e4e4e4';
+                context.strokeStyle = gridStroke;
+                context.fillStyle = gridFill;
                 drawBlock(x,y);
             }
         }
@@ -35,7 +41,7 @@ function render(){
     for(var y = 0; y < 4; ++y){
         for(var x = 0; x < 4; ++x){
             if(current[y][x]){
-                context.strokeStyle = 'black';
+                context.strokeStyle = minoStroke;
                 context.fillStyle = colors[current[y][x]-1];
                 drawBlock(curX + x, curY + y);
             }
