26ad662bec740eb8b4437745382974b06f20d5b4
LumenTeun <teun@lutic.org>
2014-06-21 03:42:27 +0100

    Added background grid.

--- render.js
+++ render.js
@@ -13,21 +13,29 @@ function drawBlock(x,y){
 function render(){
     // Clears matrix.
     context.clearRect(0,0,width,height);
-    
-    context.strokeStyle = 'black';
+
+    //
     for(var x = 0; x < cols; ++x){
         for(var y = 0; y < rows; ++y){
+            // If there's a mino there, render it...
             if(board[y][x]){
+                context.strokeStyle = 'black';
                 context.fillStyle = colors[board[y][x]-1];
+                drawBlock(x,y);
+            }
+            // ...otherwise, render the background grid.
+            else{
+                context.fillStyle = '#eeeeee';
+                context.strokeStyle = '#e4e4e4';
                 drawBlock(x,y);
             }
         }
     }
 
-    context.fillStyle = 'red';
     for(var y = 0; y < 4; ++y){
         for(var x = 0; x < 4; ++x){
             if(current[y][x]){
+                context.strokeStyle = 'black';
                 context.fillStyle = colors[current[y][x]-1];
                 drawBlock(curX + x, curY + y);
             }
